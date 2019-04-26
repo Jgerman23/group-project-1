05567aa7169626d4a22712d3cb603ab9cc4285f5
Justin German <justin.german23@gmail.com>
2019-04-25 21:07:35 -0500

    Made changes to main page, added demo.js, and styling

---
+++ assets/javascript/demo.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $(".input-group").hide();
+
+    $("button").click(function () {
+        console.log("clicked");
+        // animates the bottom portion down
+        $("#logoBtm").animate({
+            top: "500px"
+        });
+        // animates the button from bottom of the div to the right side
+        $("#bttn").animate({
+            top: "120px",
+            right: "500px",
+            opacity: "0"
+        });
+        $("#bttn").empty();
+        $(".input-group").fadeIn(2000);
+    });
+
+});
