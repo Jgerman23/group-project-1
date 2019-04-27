0ef48cdced99209561456b6257fa5a07e365f8dd
Nigel <neastern101@yahoo.com>
2019-04-27 09:38:46 -0500

    clean up youtube

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,9 @@
 $(document).ready(function () {
     console.log("ready to go");
 
-
-    
     $("#topic-search").on("click", function (event) {
-        // console.log("search");
         var topic = $("#topic-input").val().trim();
+        event.preventDefault();
         retrieveData(topic);
     });
 
@@ -13,7 +11,6 @@ $(document).ready(function () {
         console.clear();
         console.log("retrieve data: " + topic);
         
-        //https://developers.google.com/youtube/v3/docs/search/list
         var part = "snippet";
         var q = topic + "-trailer";
         var apiKey = "AIzaSyAOaGuq-aF8SJE8pVwGS_NQ7tL9-yEXn84";
@@ -23,8 +20,7 @@ $(document).ready(function () {
         var videoDefinition = "high";
         var videoLicense = "youtube";
         var safeSearch = "strict";
-        var videoCategoryId = "30"; //https://gist.github.com/dgp/1b24bf2961521bd75d6c
-
+        var videoCategoryId = "30"; 
         //together
         var type = "video";
         var videoType = "movie";
@@ -32,7 +28,6 @@ $(document).ready(function () {
         var queryURL = "https://www.googleapis.com/youtube/v3/search?part="
             + part
             + "&q=" + q
-            // + "&videoCategoryId=" + videoCategoryId
             + "&type=" + type
             + "&videoLicense=" + videoLicense
             + "&videoDefinition=" + videoDefinition
@@ -44,41 +39,35 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            var responseString = JSON.stringify(response, '', 2);
-            // console.log(responseString);
-
-            console.log(response.items.length);
-            $("#topic-result").empty();
-            for (var i = 0; i < response.items.length; i++) {
-                var data = response.items[i];
-                console.log(data.snippet.title);
-                var tRow = $("<tr>");
-                var title = $("<td>").text(data.snippet.title);
-                var kind = $("<td>").text(data.id.kind);
-                var videoId = $("<td>").text(data.id.videoId);
-
-                var iframe = $("<iframe>");
-                iframe.attr("id", "ytplayer");
-                iframe.attr("type", "text/html");
-                iframe.attr("width", "640");
-                iframe.attr("height", "360");
-                // iframe.attr("src", "https://youtube.com/watch?v=" + data.id.videoId + "&output=embed");
-                iframe.attr("src", "https://youtube.com/embed/" + data.id.videoId);
-                iframe.appendTo("body");
-                var link = $('<a>', {
-                    // text: data.snippet.title,
-                    target: '_new',
-                    href: "https://youtube.com/watch?v=" + data.id.videoId,
-                }).appendTo('body');
-
-                tRow.append(iframe);
-                
-
-
-                $("#topic-result").append(tRow);
-            }
+            displayTopic(response);
         });
     }
 
 
+    var displayTopic = function (response) {
+        $("#topic-result").empty();
+        for (var i = 0; i < response.items.length; i++) {
+            var data = response.items[i];
+            console.log(data.snippet.title);
+            var tRow = $("<tr>");
+            var title = $("<td>").text(data.snippet.title);
+            var kind = $("<td>").text(data.id.kind);
+            var videoId = $("<td>").text(data.id.videoId);
+
+            var iframe = $("<iframe>");
+            iframe.attr("id", "ytplayer");
+            iframe.attr("type", "text/html");
+            iframe.attr("width", "640");
+            iframe.attr("height", "360");
+            iframe.attr("src", "https://youtube.com/embed/" + data.id.videoId);
+            iframe.appendTo("body");
+            var link = $('<a>', {
+                target: '_new',
+                href: "https://youtube.com/watch?v=" + data.id.videoId,
+            }).appendTo('body');
+
+            tRow.append(iframe);
+            $("#topic-result").append(tRow);
+        }
+    }
 })
