889cf0c048b0d32360dfb7c63e586f3a6f5ff9ff
Mike Z <mzapata2991@gmail.com>
2019-04-27 10:53:52 -0500

    added firebase api

---
+++ assets/javascript/api.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA--3FL7nwzO3wrYMOecxPqBgfW0pvQlgI",
+    authDomain: "groupproject1-ab68f.firebaseapp.com",
+    databaseURL: "https://groupproject1-ab68f.firebaseio.com",
+    projectId: "groupproject1-ab68f",
+    storageBucket: "groupproject1-ab68f.appspot.com",
+    messagingSenderId: "982789806596"
+  };
+  firebase.initializeApp(config);
