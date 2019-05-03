7f7456947462939f55f4aba4f11b9549b02dc819
Nigel <neastern101@yahoo.com>
2019-05-03 13:53:47 -0500

    code cleanup and refactoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,79 +1,84 @@
 $(document).ready(function () {
     console.log("ready to go");
-    
     var savedSearches = [];
 
-    // listener for firebase data changes
-    database.ref("/moviebox/upcomming").on("child_added", function (data) {
-    })
-    database.ref("/moviebox/previousSearch").on("child_added", function (data) {
-    })
-
-    //search on enter
-    var input = document.getElementById("topic-input");
+    
+    //allow enter key to initiate search
+    var input = document.getElementById("movie-input");
     input.addEventListener("keyup", function (event) {
         if (event.keyCode === 13) {
             event.preventDefault();
-            document.getElementById("topic-search").click();
+            document.getElementById("movie-search").click();
         }
     });
 
-    $("#topic-search").on("click", function (event) {
-        var topic = $("#topic-input").val().trim();
+
+    //search button listen
+    $("#movie-search").on("click", function (event) {
+        var movie = $("#movie-input").val().trim();
         event.preventDefault();
-      
-        if (topic.length === 0) {
+        if (movie.length === 0) {
             $("#myModal").modal("show");
         }
         else {
-            // retrieveData(topic);
-            getYoutTubeApi(topic)
-            saveSearchFireBase(topic);
-            // movieDetails();
-            getMovieDetails(topic);
-            $("#topic-input").val("");
+            getMovieTrailer(movie)
+            saveSearchFireBase(movie);
+            getMovieDetails(movie);
+            $("#movie-input").val("");
         }
     });
 
-    var getYoutTubeApi = function (topic) {
+
+    //upcoming movie listener
+    $(document).on("click", "#movieLink", function () {
+        var movie = $(this).attr("data-name")
+        console.log("click: "+movie);
+        $(".button-container").animate({
+            top: "50px"
+        });
+        $("#bttn").animate({
+            opacity: 0,
+        }, "slow");
+        $(".search-content").fadeIn(1000);
+        getMovieTrailer(movie);
+        getMovieDetails(movie);
+    });
+
+
+    var getMovieTrailer = function (movie) {
         var apiQuery = firebase.database().ref("/moviebox/youtubeapi/");
         apiQuery.once("value").then(function (api) {
             var youTubeAPI = api.val();
-            retrieveData(topic, youTubeAPI);
+            searchYouTube(movie, youTubeAPI);
         });
     }
 
-    var getTMDBApi = function () {
+
+    var getUpComingMovies = function () {
         var apiQuery = firebase.database().ref("/moviebox/tmdbapi/");
-        apiQuery.once("value").then(function (api) {
-            var tmdbApi = api.val();
-            getUpcoming(tmdbApi);
+        apiQuery.once("value").then(function (apiKey) {
+            var tmdbAPI = apiKey.val();
+            searchTMDB(tmdbAPI);
         });
     }
 
-    var getMovieDetails = function (topic) {
+
+    var getMovieDetails = function (movie) {
         var apiQuery = firebase.database().ref("/moviebox/tmdbapi/");
-        apiQuery.once("value").then(function (api) {
-            var tmdbApi = api.val();
-            console.log(tmdbApi);
-            movieDetails(topic, tmdbApi);
+        apiQuery.once("value").then(function (apiKey) {
+            var tmdbAPI = apiKey.val();
+            movieDetails(movie, tmdbAPI);
         });
     }
 
-    var retrieveData = function (topic, youTubeAPI) {
-        console.clear();
-        console.log("retrieve data: " + topic);
 
+    var searchYouTube = function (movie, youTubeAPI) {
+        console.log("search youtube: " + movie);
         var part = "snippet";
-        var q = topic + "-trailer";
+        var q = movie;// + "-trailer";
         var maxResults = 1;
-        var channelType = "show";
-        var safeSearch = "strict";
-        var videoDefinition = "high";
+        // var videoDefinition = "high";
         var videoLicense = "youtube";
-        var safeSearch = "strict";
-        var videoCategoryId = "30";
-        //together
         var type = "video";
         var videoType = "movie";
 
@@ -82,7 +87,7 @@ $(document).ready(function () {
             + "&q=" + q
             + "&type=" + type
             + "&videoLicense=" + videoLicense
-            + "&videoDefinition=" + videoDefinition
+            // + "&videoDefinition=" + videoDefinition
             + "&videoType=" + videoType
             + "&maxResults=" + maxResults
             + "&key=" + youTubeAPI
@@ -91,18 +96,17 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            displayTopic(response);
+            showMoviePlayer(response);
         });
     }
 
 
-    var displayTopic = function (response) {
-        $("#topic-result").empty();
+    var showMoviePlayer = function (response) {
+        $("#movie-result").empty();
         for (var i = 0; i < response.items.length; i++) {
             var data = response.items[i];
             console.log(data.snippet.title);
             var tRow = $("<tr>");
-
             var iframe = $("<iframe>");
             iframe.attr("id", "ytplayer");
             iframe.attr("type", "text/html");
@@ -111,23 +115,12 @@ $(document).ready(function () {
             iframe.attr("src", "https://youtube.com/embed/" + data.id.videoId);
             iframe.appendTo("body");
             tRow.append(iframe);
-            $("#topic-result").append(tRow);
+            $("#movie-result").append(tRow);
             $(".search-result").slideDown(500)
         }
-
     }
 
 
-    // these are saved to local storate
-    var saveSearchLocal = function (topic) {
-        console.log("save search local: " + topic);
-        previousSearches.push(topic);
-        localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
-        displaySearches(previousSearches);
-        saveSearchFireBase(previousSearches);
-    }
-
-    
     var saveSearchFireBase = function (search) {
         console.log("save searh firebase");
         if (!savedSearches.includes(search)) {
@@ -144,20 +137,17 @@ $(document).ready(function () {
         var query = firebase.database().ref("/moviebox/previousSearch/");
         $(".previousSearch").empty();
         query.once("value").then(function (searchedMovie) {
-            // var table = "<table border='1|1'>";
             searchedMovie.forEach(function (childsearchedMovie) {
                 var value = childsearchedMovie.val();
                 savedSearches.push(value);
-
                 var td = $("<td>");
-                    td.html(value);
+                td.html(value);
                 var tr = $("<tr>");
-                    tr.append(td);
-                $(".previousSearch").append(tr);
+                tr.append(td);
+                $(".previousSearch").prepend(tr);
             });
         });
     }
-    // /**************MOVIE DATABASE API************************************************ */
 
 
     var saveUpComming = function (movies) {
@@ -175,153 +165,95 @@ $(document).ready(function () {
         }
     }
 
-    function getUpcoming(tmdbApi) {
 
-        var queryURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=" + tmdbApi + "&language=en-US&page=1";
-
+    function searchTMDB(tmdbAPI) {
+        var queryURL = "https://api.themoviedb.org/3/movie/upcoming?api_key=" +
+            tmdbAPI + "&language=en-US&page=1";
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-
             var results = response.results;
-            console.log(results);
             saveUpComming(results);
-
-            for (var i = 0; i < 5; i++) {
-                var name = results[i].title
-                console.log(name);
-                var a = $("<a>");
-                a.attr("href", "#")
-                console.log(a);
-                a.attr("id", "movieLink");
-                a.attr("data-name", results[i].title);
-                a.attr("title", name);
-                a.text(results[i].title);
-                var td = $("<td>");
-                td.html("<br>" + "Release Date: " + results[i].release_date);
-                var tr = $("<tr>");
-                td.prepend(a);
-                tr.append(td);
-                $(".upcomingCard").append(tr);
-
-            }
-
+            displayUpcomingMovies(results);
         });
     }
-    // getUpcoming();
-    getTMDBApi();
-    displaySearches();
-    
-    function movieDetails(topic, tmdbApi) {
+
+
+    var displayUpcomingMovies = function (results) {
+        for (var i = 0; i < 7; i++) {
+            var name = results[i].title
+            var a = $("<a>");
+            a.attr("href", "#")
+            a.attr("id", "movieLink");
+            a.attr("data-name", results[i].title);
+            a.attr("title", name);
+            a.text(results[i].title);
+            var td = $("<td>");
+            td.html("<br>" + "Release Date: " + results[i].release_date);
+            var tr = $("<tr>");
+            td.prepend(a);
+            tr.append(td);
+            $(".upcomingCard").append(tr);
+        }
+    }
+
+
+    function movieDetails(movie, tmdbAPI) {
         console.log("movie details");
-
-        var movieSearch = $("#topic-input").val().trim();
-        var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbApi + "&language=en-US&query=" +
-            topic + "&page=1&include_adult=false";
-
+        var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=" +
+            tmdbAPI + "&language=en-US&query=" +
+            movie + "&page=1&include_adult=false";
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-
             var detailResults = response.results;
-            console.log(detailResults);
-
-
-            for (var i = 0; i < 1; i++) {
-                var movieTitle = detailResults[i].title;
-                var movieRating = detailResults[i].vote_average;
-                var releaseDate = detailResults[i].release_date;
-                var overviewResults = detailResults[i].overview;
-
-                $("#movieTi").text(movieTitle);
-                $("#movieRa").text(movieRating);
-                $("#movieRD").text("Release Date: " + releaseDate);
-                $("#movieOv").text("Movie Overview: " + overviewResults);
-                console.log(overviewResults);
-                document.getElementById("stars").innerHTML = getStars(movieRating);
-            }
+            displayMovieDetails(detailResults);
         })
     }
 
-    $(document).on("click", "#movieLink", function () {
-        console.log("click");
-        var movieSearch = $("#movieLink").val();
-        var movie = $(this).attr("data-name")
-        console.log(movie);
-        $(".button-container").animate({
-            top: "50px"
-        });
-        $("#bttn").animate({
-            opacity: 0,
-        }, "slow");
-        $(".search-content").fadeIn(1000);
-        // movieClickLink(movie)
-        getMovieDetails(movie);
-        // retrieveData(movie)
-        getYoutTubeApi(movie);
-        console.log(movieSearch);
-    });
 
-    function movieClickLink(movie) {
+    var displayMovieDetails = function (detailResults) {
+        for (var i = 0; i < 1; i++) {
+            var movieTitle = detailResults[i].title;
+            var movieRating = detailResults[i].vote_average;
+            var releaseDate = detailResults[i].release_date;
+            var overviewResults = detailResults[i].overview;
 
-
-        var movieSearch = $("#movieLink").val();
-        console.log(movieSearch);
-        var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=" + apiKey2 + "&language=en-US&query=" +
-            movie + "&page=1&include_adult=false";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-
-            var detailResults = response.results;
-            console.log(detailResults);
-
-
-            for (var i = 0; i < 1; i++) {
-                var movieTitle = detailResults[i].title;
-                var movieRating = detailResults[i].vote_average;
-                var releaseDate = detailResults[i].release_date;
-                var overviewResults = detailResults[i].overview;
-
-                $("#movieTi").text(movieTitle);
-                $("#movieRa").text(movieRating);
-                $("#movieRD").text("Release Date: " + releaseDate);
-                $("#movieOv").text("Movie Overview: " + overviewResults);
-                console.log(overviewResults);
-                document.getElementById("stars").innerHTML = getStars(movieRating);
-            }
-        });
+            $("#movieTi").text(movieTitle);
+            $("#movieRa").text(movieRating);
+            $("#movieRD").text("Release Date: " + releaseDate);
+            $("#movieOv").text("Movie Overview: " + overviewResults);
+            document.getElementById("stars").innerHTML = getStars(movieRating);
+        }
     }
 
 
     function getStars(rating) {
-
         // Round to nearest half
         rating = Math.round(rating * 2) / 2;
         let output = [];
-
         // Append all the filled whole stars
-        for (var i = rating; i >= 1; i--)
+        for (var i = rating; i >= 1; i--) {
             output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-
+        }
         // If there is a half a star, append it
-        if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-
+        if (i == .5) {
+            output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+        }
         // Fill the empty stars
-        for (let i = (5 - rating); i >= 1; i--)
+        for (let i = (5 - rating); i >= 1; i--) {
             output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-
+        }
         return output.join('');
-
     }
 
+
+    getUpComingMovies();
+    displaySearches();
 });
 
 
